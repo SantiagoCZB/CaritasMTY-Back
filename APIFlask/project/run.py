ba796f46a48cbccc463f77fa80f3f70243573b91
A01198951 <A01198951@tec.mx>
2024-10-08 09:41:35 -0600

    Cert

--- APIFlask/project/run.py
+++ APIFlask/project/run.py
@@ -2,5 +2,10 @@ from app import create_app
 
 app = create_app()
 
+API_CERT = '/home/user01/realmadswift.tc2007b.tec.mx.cer'
+API_KEY = '/home/user01/realmadswift.tc2007b.tec.mx.key'
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+	import ssl
+	context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+	context.load_cert_chain(API_CERT, API_KEY)
+	app.run(host='0.0.0.0', port=10206, ssl_context=context, debug=True)
