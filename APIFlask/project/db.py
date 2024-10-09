0f9e1c6786af58e15d9cb9e4bbb0d601dbf989e7
SantiagoCZB <s.carrizalesb04@gmail.com>
2024-10-09 10:22:33 -0600

    prueba3

--- APIFlask/project/db.py
+++ APIFlask/project/db.py
@@ -7,16 +7,12 @@ load_dotenv()
 
 def get_db_connection():
     try:
-        
-        print(f"DB_SERVER: {os.getenv('DB_SERVER')}")
-        print(f"DB_USER: {os.getenv('DB_USER')}")
-        print(f"DB_PASSWORD: {os.getenv('DB_PASSWORD')}")
-        print(f"DB_NAME: {os.getenv('DB_NAME')}")
+
         conn = pymssql.connect(
             server=os.getenv('DB_SERVER'),
             user=os.getenv('DB_USER'),
             password=os.getenv('DB_PASSWORD'),
-            database=os.getenv('DB_NAME'),
+            database='CaritasDB',
             port=1433
         )
         return conn
